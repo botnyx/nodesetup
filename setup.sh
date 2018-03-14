178fdcc4f601ea3419ccab7cd1848648972f785c
botnyx <chlsbkrs@gmail.com>
2018-03-15 00:15:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,20 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 sudo apt-get update
 sudo apt-get -y install docker-ce cifs-utils
 
+sudo curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 sudo echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 
-sudo apt-get -y install apt-transport-https
+sudo apt-get -y install apt-transport-https mlocate
 sudo apt-get update
 sudo apt-get -y install webmin
+
+sudo mkdir -p /srv/docker/system
+sudo mkdir -p /srv/docker/services
+sudo mkdir -p /srv/docker/sites
+
+
+
