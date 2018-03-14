096600e28fb6f43a11a967c978f25c1a4b8336ca
botnyx <chlsbkrs@gmail.com>
2018-03-14 20:24:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get -y install docker-ce
 
 
 sudo deb https://download.webmin.com/download/repository sarge contrib
 wget http://www.webmin.com/jcameron-key.asc
 sudo apt-key add jcameron-key.asc
 
-sudo apt-get install apt-transport-https
+sudo apt-get -y install apt-transport-https
 sudo apt-get update
-sudo apt-get install webmin
+sudo apt-get -y install webmin
