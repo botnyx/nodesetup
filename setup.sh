adfd52c1ce0fd671d44096b0ec8a411aa2280105
botnyx <chlsbkrs@gmail.com>
2018-03-14 21:39:23 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get -y install docker-ce
+sudo apt-get -y install docker-ce cifs-utils
 
 sudo echo "deb https://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
 wget http://www.webmin.com/jcameron-key.asc
