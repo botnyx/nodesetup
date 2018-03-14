b1f2a601a0e77d71bf18c330785b2941402b2201
botnyx <chlsbkrs@gmail.com>
2018-03-14 20:17:35 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,12 @@ sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get install docker-ce
+
+
+deb https://download.webmin.com/download/repository sarge contrib
+wget http://www.webmin.com/jcameron-key.asc
+apt-key add jcameron-key.asc
+
+apt-get install apt-transport-https
+apt-get update
+apt-get install webmin
