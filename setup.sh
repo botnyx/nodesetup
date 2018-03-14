76d0066e76ac2b292e98d51ca344bee41fe6f032
botnyx <chlsbkrs@gmail.com>
2018-03-14 20:15:33 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
